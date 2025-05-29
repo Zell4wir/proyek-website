fa0b9fa2c8fe23a5d707548d6fc095ef9e5a1cc9
Zell4wir <fazelmawla13@gmail.com>
2025-05-29 23:02:36 +0700

    ubah sedikit

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay, Navigation } from "swiper/modules";
-import "swiper/css";
+import "swiper/css";  
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
