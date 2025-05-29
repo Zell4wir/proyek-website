14c678b35329cbc4199a2ad3ce665c83fe616e1b
Zell4wir <fazelmawla13@gmail.com>
2025-05-30 04:41:49 +0700

    perubahan icon web

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/homepage.jsx";
-import ContactPage from "./pages/contact.jsx";
 import CostumerService from "./pages/costumer-service.jsx";
-import ProdukPage from "./pages/menu-page.jsx";
 import MenuPage from "./pages/menu-page.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -13,7 +11,6 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/costumer-service" element={<CostumerService />} />
         <Route path="/*" element={<div>404 Not Found</div>} />
